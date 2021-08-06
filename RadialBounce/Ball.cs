4bf4eb24021be9f2f12c38be03e0cd2cb8725f1e
Zanlin Oo <zanlin87@gmail.com>
2021-08-06 10:31:56 -0700

    Add project files.

---
+++ RadialBounce/Ball.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Drawing;
+
+namespace RadialBounce
+{
+    public struct Point
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public Point(float x, float y)
+        {
+            X = x;
+            Y = y; 
+        }
+    }
+
+
+    public class Ball
+    {
+        private int diameter;
+        private Color color;
+        private float x;
+        private float y;
+
+        private Point Speed;
+        public Point Location
+        {
+            get
+            {
+                return new Point(x, y);
+            }       
+        }
+
+        public int Diameter
+        {
+            get
+            {
+                return diameter;
+            }
+        }
+
+        public Ball(float x, float y, float speed, int diameter, Color color)
+        {
+            this.x = x;
+            this.y = y;
+            this.Speed = new Point(speed, speed);
+            this.diameter = diameter;
+            this.color = color;
+        }
+        void BounceOffOutSide(Ball ball)
+        {
+            int deltaX = (int)(ball.Location.X - x);
+            int deltaY = (int)(ball.Location.Y - y);
+            int deltaDistance = (int)Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+            
+            if (deltaDistance >= ball.Diameter / 2 - diameter / 2)
+            {
+                KeepInSquareBoundary(new Rectangle((int)(ball.Location.X - deltaDistance / 2), (int)(ball.Location.Y - deltaDistance / 2), deltaDistance, deltaDistance));
+            }
+
+        }
+        private void BounceOffInside(Ball ball)
+        {
+            int deltaX = (int)(ball.Location.X - x);
+            int deltaY = (int)(ball.Location.Y - y);
+            int deltaDistance = (int)Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+
+            if (deltaDistance <= ball.Diameter / 2 + diameter / 2)
+            {
+                KeepOutOfSquareBoundary(new Rectangle((int)ball.Location.X, (int)ball.Location.Y, ball.Diameter, ball.Diameter));
+            }
+
+        }
+        private void KeepOutOfSquareBoundary(Rectangle boundary)
+        {
+            int radius = diameter / 2;
+            if (x - radius + diameter >= boundary.Width)
+            {
+                Speed.X = Math.Abs(Speed.X);
+            }
+            else if (x - radius <= boundary.X)
+            {
+                Speed.X = -Math.Abs(Speed.X);
+            }
+
+            if (y - radius + diameter >= boundary.Height)
+            {
+                Speed.Y = Math.Abs(Speed.Y);
+            }
+            else if (y - radius <= boundary.Y)
+            {
+                Speed.Y = -Math.Abs(Speed.Y);
+            }
+        }
+
+        private void KeepInSquareBoundary(Rectangle boundary)
+        {
+            int radius = diameter / 2;
+            if (x + radius >= boundary.X + boundary.Width)
+            {
+                Speed.X = -Math.Abs(Speed.X);
+            }
+            else if (x - radius <= boundary.X)
+            {
+                Speed.X = Math.Abs(Speed.X);
+            }
+
+            if (y + radius >= boundary.Y + boundary.Height)
+            {
+                Speed.Y = -Math.Abs(Speed.Y);
+            }
+            else if(y - radius <= boundary.Y)
+            {
+                Speed.Y = Math.Abs(Speed.Y);
+            }
+
+        }
+        private void MatchSpeed(Ball ball)
+        {
+            x += Speed.X;
+            y += Speed.Y;
+
+            Speed = ball.Speed;
+        }
+        public void Update(Ball ball)
+        {
+            MatchSpeed(ball);
+        }
+        public void Update(Rectangle squareBoundary)
+        {
+            x += Speed.X;
+            y += Speed.Y;
+
+            KeepInSquareBoundary(squareBoundary);
+        }
+        public void Update(Rectangle squareBoundary, Ball innerBall, Ball outerBall)
+        {
+            x += Speed.X;
+            y += Speed.Y;
+
+            KeepInSquareBoundary(squareBoundary);
+            BounceOffInside(innerBall);
+            BounceOffOutSide(outerBall);
+        }
+
+        public void Update(Rectangle squareBoundary, Ball ball)
+        {
+            x += Speed.X;
+            y += Speed.Y;
+
+            KeepInSquareBoundary(squareBoundary);
+            BounceOffInside(ball);
+        }
+        public void Draw(Graphics graphics)
+        {            
+            graphics.FillEllipse(new SolidBrush(color), x - diameter / 2, y - diameter / 2, diameter, diameter);
+        }
+    }
+}
---
+++ RadialBounce/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadialBounce
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        private Bitmap image;
+
+        Ball[] balls;
+        Ball innerBall;
+        Ball outerBall;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            balls = new Ball[1000];
+
+            Random random = new Random();
+
+            for (int i = 0; i < balls.Length; i++)
+            {
+                balls[i] = new Ball(x: random.Next(0, Canvas.Width), y: random.Next(0, Canvas.Height), speed: 3f, diameter: 10, color: Color.DarkGoldenrod);
+            }
+            
+            innerBall = new Ball(x: Canvas.Width - 200, y: Canvas.Height - 200, speed: 0.5f, diameter: 270, color: Color.LightSeaGreen);
+            outerBall = new Ball(x: innerBall.Location.X, y: innerBall.Location.Y, speed: 0.5f, diameter: 300, color: Color.PaleVioletRed);
+
+            Canvas.BackColor = Color.CornflowerBlue;
+
+            image = new Bitmap(Canvas.Width, Canvas.Height);
+            graphics = Graphics.FromImage(image);
+
+            Canvas.Image = image;
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            graphics.Clear(Canvas.BackColor);
+            UpdateObjects();
+            Draw();
+            Canvas.Image = image;
+        }
+
+        private void UpdateObjects()
+        {
+            outerBall.Update(new Rectangle(Canvas.Location, Canvas.Size));
+            innerBall.Update(outerBall);
+
+            foreach (var ball in balls)
+            {
+                ball.Update(new Rectangle(Canvas.Location, Canvas.Size), innerBall, outerBall);
+            }
+        }
+
+        private void Draw()
+        {
+            outerBall.Draw(graphics);
+            innerBall.Draw(graphics);
+
+            foreach (var ball in balls)
+            {
+                ball.Draw(graphics);
+            }
+        }
+    }
+}
